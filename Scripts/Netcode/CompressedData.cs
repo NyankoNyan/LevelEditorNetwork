5d9604819daa3674c53fe3b503f2408d6f9110cc
Nyanko Nyan <gverycrazy@gmail.com>
2024-10-26 03:04:59 +0300

    Some changes

---
+++ Scripts/Netcode/CompressedData.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Unity.Netcode;
+using Unity.SharpZipLib.GZip;
+
+namespace LevelNet.Netcode
+{
+    public class CompressedData
+    {
+        private object _data;
+
+        public CompressedData(object data)
+        {
+            _data = data;
+        }
+
+        public CompressedData(FastBufferReader reader)
+        {
+            Read(reader);
+        }
+
+        public override string ToString()
+        {
+            if (_data != null)
+            {
+                return _data.ToString();
+            }
+            else
+            {
+                return "null";
+            }
+        }
+
+        public void Read(FastBufferReader packedReader)
+        {
+            // Read zip data
+            packedReader.ReadValueSafe(out int zipDataSize);
+            byte[] zipData = new byte[zipDataSize];
+            packedReader.ReadBytesSafe(ref zipData, zipDataSize);
+
+            byte[] unpackedData = UnpackData(zipData);
+            zipData = null;
+
+            // Deserialize data as usuial
+            FastBufferReader unpackedReader = new(unpackedData, Unity.Collections.Allocator.Temp, unpackedData.Length, 0);
+            _data = NetcodeStaticSerializer.Instance.Deserialize(unpackedReader);
+        }
+
+        private static byte[] UnpackData(byte[] zipData)
+        {
+            const int bufferSize = 1024;
+
+            int readLen = 0;
+            int totalReadSize = 0;
+            byte[] buffer = new byte[bufferSize];
+            List<byte[]> unpackedParts = new();
+
+            // Unzip data
+            using (var ms = new MemoryStream(zipData))
+            {
+                using (GZipInputStream inputStream = new(ms))
+                {
+                    while ((readLen = inputStream.Read(buffer)) > 0)
+                    {
+                        if (readLen == bufferSize)
+                        {
+                            unpackedParts.Add(buffer);
+                        }
+                        else
+                        {
+                            byte[] lastPart = new byte[readLen];
+                            Array.Copy(buffer, lastPart, readLen);
+                            unpackedParts.Add(lastPart);
+                        }
+                        totalReadSize += readLen;
+                    }
+                }
+            }
+
+            // Merge unziped data parts
+            byte[] unpackedData = new byte[totalReadSize];
+            int currentSize = 0;
+            foreach (var dataPart in unpackedParts)
+            {
+                Array.Copy(dataPart, 0, unpackedData, currentSize, dataPart.Length);
+                currentSize += dataPart.Length;
+            }
+
+            return unpackedData;
+        }
+
+        public void Write(FastBufferWriter packedWriter)
+        {
+            // Serialize data without compression
+            FastBufferWriter unpackedWriter = new(1024, Unity.Collections.Allocator.Temp, 65536);
+            NetcodeStaticSerializer.Instance.Serialize(_data, unpackedWriter);
+            byte[] unpackedData = unpackedWriter.ToArray();
+            byte[] packedData = null;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (GZipOutputStream outStream = new GZipOutputStream(ms))
+                {
+                    outStream.Write(unpackedData, 0, unpackedData.Length);
+                }
+                packedData = ms.ToArray();
+            }
+
+            packedWriter.WriteValueSafe(packedData.Length);
+            packedWriter.WriteBytesSafe(packedData);
+        }
+    }
+}
---
+++ Tests/Scripts/GameInitializer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using LevelNet.Data;
+using LevelView;
+using UnityEngine;
+
+namespace LevelNet
+{
+    public class NetEventException : Exception
+    {
+        public NetEventException(string message):base(message) { }
+    }
+
+    public class DataCreatedEventArgs
+    {
+
+    }
+    public interface INetEvents
+    {
+        delegate void StartAsServerDelegate();
+        delegate void StartAsClientDelegate();
+        delegate void DataCreatedDelegate(DataCreatedEventArgs args);
+        bool IsOnline { get; }
+        bool IsServer { get; }
+
+        StartAsServerDelegate OnStartAsServer { get; set; }
+        StartAsClientDelegate OnStartAsClient { get; set; }
+        DataCreatedDelegate OnDataCreated { get; set; }
+
+        void StartDataReceiver();
+
+        void RegDataContainer(SyncDataContainer dataContainer);
+    }
+
+
+
+    public static class NetEventsFabric
+    {
+        private static INetEvents _netEvents;
+
+        public static INetEvents Create() => _netEvents ??= new Netcode.NetcodeEvents();
+    }
+}
+
+namespace LevelNet.Netcode
+{
+}
+
+namespace LevelNet.Tests
+{
+    public class GameInitializer : MonoBehaviour
+    {
+        private void Start()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            INetEvents netEvents = NetEventsFabric.Create();
+            if (netEvents.IsOnline)
+            {
+                if (netEvents.IsServer) {
+                    CreateData();
+                }
+                else
+                {
+                    netEvents.OnDataCreated = (args) => SyncView();
+                    netEvents.StartDataReceiver();
+                }
+            }
+            else
+            {
+                netEvents.OnStartAsServer = ()=> CreateData();
+                netEvents.OnStartAsClient = () =>
+                {
+                    netEvents.OnDataCreated = (args) => SyncView();
+                    netEvents.StartDataReceiver();
+                };
+            }
+        }
+
+        private void CreateData()
+        {
+
+        }
+
+        private void SyncView()
+        {
+
+        }
+
+    }
+}
